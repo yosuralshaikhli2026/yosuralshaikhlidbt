f9c8ea5d999a2260198d87a6d2177196e431b0b9
motez_adnan@yahoo.com <motez_adnan@yahoo.com>
2026-05-06 18:01:02 +0000

    table and view materlization

---
+++ models/demo/customer.sql
@@ -0,0 +1,22 @@
+{{
+    config 
+    (
+       materialized = 'table'
+    )
+}}
+
+
+with customer_src as
+(
+    select 
+    CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    PHONE,
+    COUNTRY,
+    CREATED_AT,
+    CURRENT_TIMESTAMP AS INSERT_DTS
+    FROM {{ source('customer','CUSTOMER_SRC')}} 
+)
+SELECT * FROM customer_src
