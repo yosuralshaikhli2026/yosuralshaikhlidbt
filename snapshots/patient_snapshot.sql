650f5ce86e4d36b8661526d8b0c8da742998d022
motez_adnan@yahoo.com <motez_adnan@yahoo.com>
2026-05-08 06:36:27 +0000

    snapshot

---
+++ snapshots/patient_snapshot.sql
@@ -0,0 +1,19 @@
+{% snapshot patient_snapshot %}
+
+
+{{
+    config
+    (
+        strategy = 'check',
+        unique_key = 'PATIENT_ID',
+        check_cols = ['PATIENT_NAME','PATIENT_CONTACT_NUMBER','PATIENT_EMAIL_ID','PATIENT_ADDRESS']
+    )
+}}
+
+
+select * from {{source('patient','PATIENT_SRC')}}
+
+
+
+
+{% endsnapshot %} 
